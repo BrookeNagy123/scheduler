9b3c34cfad291b83f7f79aca5cf0060abf24ef3d
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-03-05 12:38:50 -0700

    Add helper functions and testing

---
+++ src/helpers/selectors.js
@@ -0,0 +1,46 @@
+export function getAppointmentsForDay(state, day) {
+  const result = [];
+  const selectedDay = state.days.filter(d => d.name === day)[0];
+  if(!selectedDay){
+    return result;
+  }
+  const appointments = selectedDay.appointments
+  for(const appointmentID of appointments){
+    if(state.appointments[appointmentID]){
+      result.push(state.appointments[appointmentID])
+    }
+  }
+  return result;
+}
+
+//This function will return an object that contains the interview data if it is passed an object that contains an interviewer.
+
+export function getInterview(state, interview){
+if(!interview){
+    return null;
+}
+const results = {}; 
+const interviewers = state.interviewers
+for(const key in interviewers){
+  if(interviewers[key].id === interview.interviewer){
+    results["interviewer"] = interviewers[key]
+    results["student"] = interview.student
+    return results; 
+  }
+}
+}
+
+export function getInterviewersForDay(state, day){
+  const result = [];
+  const selectedDay = state.days.filter(d => d.name === day)[0];
+  if(!selectedDay){
+    return result;
+  }
+  const interviewer = selectedDay.interviewers
+  for(const interviewerID of interviewer){
+    if(state.interviewers[interviewerID]){
+      result.push(state.interviewers[interviewerID])
+    }
+  }
+  return result;
+}
