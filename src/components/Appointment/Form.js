f7742043131be0fecd31209e2775bfc01302db99
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-03-11 17:05:04 -0700

    Remove hard-coded interviewers and update interviewers to be props.interviewers

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -2,14 +2,6 @@ import React, { useState } from 'react';
 import Button from "components/Button"
 import InterviewerList from 'components/InterviewerList';
 
-const interviewers = [
-  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-];
-
 export default function Form(props){
 const [student, setStudent] = useState(props.student || "");
 const [interviewer, setInterviewer] = useState(props.interviewer || null);
@@ -21,6 +13,7 @@ const cancel = () => {
   reset()
   props.onCancel()
 }
+
   return <main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
     <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
@@ -34,7 +27,7 @@ const cancel = () => {
       />
     </form>
      <InterviewerList 
-      interviewers={interviewers}
+      interviewers={props.interviewers}
       value={interviewer}
       onChange={setInterviewer}
     /> 
