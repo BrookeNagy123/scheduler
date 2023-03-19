e9ae08c494445a22bd20f7944a6d9a4a36936d21
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-03-19 14:49:33 -0600

    Add data-testid tag

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -15,7 +15,7 @@ export default function DayListItem(props) {
 
   const dayClass = classNames("day-list__item", {"day-list__item--selected ":props.selected, "day-list__item--full":props.spots === 0});
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected}>
+    <li data-testid="day" className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected} >
       <h2>{props.name}</h2>
       {formatSpots()} 
     </li>
