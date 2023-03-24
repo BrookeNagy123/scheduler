d6bb89cead1d86032d48ad20a68183d95d9772a6
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-03-24 16:50:43 -0600

    Add a comment for the prop types check

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,21 +1,21 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
-import "components/InterviewerList.scss"
-import classNames from "classnames";
+import "components/InterviewerList.scss";
 import PropTypes from 'prop-types';
 
 export default function InterviewerList(props) {
   const interviewer = props.interviewers?.map((interviewer) =>  {
     return (
-  <InterviewerListItem key={interviewer.id} name={interviewer.name} avatar={interviewer.avatar} selected={interviewer.id === props.value} setInterviewer={() => props.onChange(interviewer.id)}  />
-  )
-})
+      <InterviewerListItem key={interviewer.id} name={interviewer.name} avatar={interviewer.avatar} selected={interviewer.id === props.value} setInterviewer={() => props.onChange(interviewer.id)}  />
+    );
+  });
   return  <section className="interviewers">
-  <h4 className="interviewers__header text--light">Interviewer</h4>
-  <ul className="interviewers__list">{interviewer}</ul>
-</section>
+    <h4 className="interviewers__header text--light">Interviewer</h4>
+    <ul className="interviewers__list">{interviewer}</ul>
+  </section>;
 }
 
+//Check on the Props Type. For Interviewers an array is required. 
 InterviewerList.propTypes = {
   interviewers: PropTypes.array.isRequired
 };
