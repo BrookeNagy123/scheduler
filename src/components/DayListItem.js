cddb9c8166bc6f439c3d2ef87f3422b523d44417
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-03-24 16:49:55 -0600

    Add a comment to indicate what the function formatSpots is for

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,13 +3,14 @@ import "components/DayListItem.scss";
 import classNames from "classnames";
 
 export default function DayListItem(props) {
-  function formatSpots(){
-    if(props.spots === 0){
-      return  <h3>no spots remaining</h3> 
-    } else if (props.spots === 1){
-      return <h3>{props.spots} spot remaining</h3>  
+  //Format the number of spots correctly based on the number of spots remaining
+  function formatSpots() {
+    if (props.spots === 0) {
+      return  <h3>no spots remaining</h3>;
+    } else if (props.spots === 1) {
+      return <h3>{props.spots} spot remaining</h3>;
     } else {
-      return <h3>{props.spots} spots remaining</h3>  
+      return <h3>{props.spots} spots remaining</h3>;
     }
   }
 
@@ -17,7 +18,7 @@ export default function DayListItem(props) {
   return (
     <li data-testid="day" className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected} >
       <h2>{props.name}</h2>
-      {formatSpots()} 
+      {formatSpots()}
     </li>
   );
-  }
+}
