0b4ec3cadca18252975f5bd7f4acf1e9b785d502
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-03-24 16:44:05 -0600

    Add comment to indicate what custom hook is for and did a clean up on sspacing

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,24 +1,25 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
+//Custom hook to transition or go back between different modes. 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  function transition (mode, replace = false) {
-    if(replace === true){
+  function transition(mode, replace = false) {
+    if (replace === true) {
       setHistory(prev => ([...prev.slice(0, prev.length - 1), mode]));
       setMode(mode);
       return;
     } else {
-    setMode(mode)
-    setHistory(prev => ([...prev, mode]))
+      setMode(mode);
+      setHistory(prev => ([...prev, mode]));
     }
-    }
-  function back (){
-    if(history.length <= 1){
-      setMode(history[0])
+  }
+  function back() {
+    if (history.length <= 1) {
+      setMode(history[0]);
     } else {
-    history.pop()
-    setMode(history[history.length - 1]) 
+      history.pop();
+      setMode(history[history.length - 1]);
     }
   }
   return {mode, transition, back};
