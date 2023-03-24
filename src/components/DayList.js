bd8edbe8ff87771afa980c33dfb4e3837812bfba
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-03-24 16:47:14 -0600

    Clean up the spacing in the DayList function

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,8 +4,8 @@ import DayListItem from "./DayListItem";
 export default function DayList(props) {
   const day = props.days.map((day) =>  {
     return (
-    <DayListItem key={day.id} name={day.name} spots={day.spots} selected={day.name === props.value} setDay={props.onChange} />
-  )
-})
-  return <ul> {day}</ul>
+      <DayListItem key={day.id} name={day.name} spots={day.spots} selected={day.name === props.value} setDay={props.onChange} />
+    );
+  });
+  return <ul> {day}</ul>;
 }
