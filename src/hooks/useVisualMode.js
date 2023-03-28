39c2abf5e732af3afee0687aafe745b1cfc6f9a8
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-03-28 17:32:58 -0600

    Refactor useVisualMode to use better varible names and update of state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,26 +1,28 @@
 import { useState } from "react";
 
-//Custom hook to transition or go back between different modes. 
+//Custom hook to transition or go back between different modes.
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  function transition(mode, replace = false) {
-    if (replace === true) {
-      setHistory(prev => ([...prev.slice(0, prev.length - 1), mode]));
-      setMode(mode);
+  const transition = (newMode, replace = false) => {
+    if (replace) {
+      setHistory(prev => ([...prev.slice(0, prev.length - 1), newMode]));
+      setMode(newMode);
       return;
     } else {
-      setMode(mode);
-      setHistory(prev => ([...prev, mode]));
+      setMode(newMode);
+      setHistory(prev => ([...prev, newMode]));
     }
-  }
-  function back() {
+  };
+  const back = () => {
     if (history.length <= 1) {
       setMode(history[0]);
     } else {
-      history.pop();
-      setMode(history[history.length - 1]);
+      let newHistory = [...history]
+      newHistory.pop();
+      setHistory(newHistory)
+      setMode(newHistory[newHistory.length - 1]);
     }
-  }
+  };
   return {mode, transition, back};
 }
