21af98864c907b7e9f635e28b19b390e2255bb94
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-03-28 17:33:37 -0600

    Refactor to use arrow function on line 7

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,7 +4,7 @@ import classNames from "classnames";
 
 export default function DayListItem(props) {
   //Format the number of spots correctly based on the number of spots remaining
-  function formatSpots() {
+  const formatSpots = () => {
     if (props.spots === 0) {
       return  <h3>no spots remaining</h3>;
     } else if (props.spots === 1) {
@@ -12,7 +12,7 @@ export default function DayListItem(props) {
     } else {
       return <h3>{props.spots} spots remaining</h3>;
     }
-  }
+  };
 
   const dayClass = classNames("day-list__item", {"day-list__item--selected ":props.selected, "day-list__item--full":props.spots === 0});
   return (
